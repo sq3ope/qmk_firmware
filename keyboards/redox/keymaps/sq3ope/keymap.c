1da29ea07c527031915c0e7f9493eea6593f4d23
Paweł Firlej <pawel.firlej@Pawes-MBP.lan>
2023-05-06 16:25:17 +0200

    Use CTRL, CMD and ALT keys instead of right hand arrows.

--- keyboards/redox/keymaps/sq3ope/keymap.c
+++ keyboards/redox/keymaps/sq3ope/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_PGDN ,        KC_HOME ,KC_ADEN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LGUI ,KC_PPLS ,KC_PMNS ,KC_ALAS ,     KC_CTPL ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,KC_SPC  ,    KC_RALT ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
+     KC_LGUI ,KC_PPLS ,KC_PMNS ,KC_ALAS ,     KC_CTPL ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,KC_SPC  ,    KC_CTPL ,     KC_ALAS ,KC_DOWN ,KC_UP   ,KC_LGUI
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
--- quickstart.md
+++ quickstart.md
@@ -3,9 +3,22 @@
 Follow the instrucions at: https://docs.qmk.fm/#/getting_started_build_tools
 
 Configure the defaults:
-> `qmk config user.keyboard=redox/rev1/base`
-> `qmk config user.keymap=sq3ope`
+1. `qmk config user.keyboard=redox/rev1/base`
+1. `qmk config user.keymap=sq3ope`
 
+# Editing my keymap
+
+Edit the file `~/qmk_firmware/keyboards/redox/keymaps/sq3ope/keymap.c`
 # Building Firmware
 
-`qmk compile -km default`
+`qmk compile`
+
+# Flashing the firmware
+1. `qmk flash`
+1. Wait until you see
+    > Waiting for USB serial port - reset your controller now ......
+1. Press layer3 (_adjust) button + Q button
+1. Wait for message
+    > avrdude done.  Thank you.
+
+Now connect the other half of keyboard by USB and repeat. Note that now you have to use P button instead of Q to reset the controller in step 3.
